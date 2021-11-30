f3fa310b571d4e43b0d151277d523ff90e23a5cf
EseAlli <essealli.ea@gmail.com>
2021-11-30 23:34:32 +0100

    exercise 3.13

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 const cors =  require('cors');
-const { response } = require('express');
+const Person = require("./models/person")
 
 app.use(cors())
 app.use(express.json());
@@ -13,46 +14,27 @@ morgan.token("content", (req) => {
   });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
 
-let persons = [
-        { 
-          "id": 1,
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": 2,
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": 3,
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": 4,
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
-    
-]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+    Person.find({}).then(persons =>{
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = parseInt(request.params.id)
-    let person = persons.filter(per=> per.id === id)
-    if (person.length > 0){
-        response.json(person)
-    } else{
-        response.status(400).send("Invalid id, user doesn't exist")
-    }   
+    const id = request.params.id
+    Person.findById(id).then(person=>{
+        if(person){
+            response.json(person)
+        }
+        else{
+            response.status(400).send("Invalid id, user doesn't exist")
+        }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response)=>{
