68810b2fa96eb5822e2e33feacef988a2805759c
EseAlli <essealli.ea@gmail.com>
2021-11-30 14:12:46 +0100

    ..

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+const cors =  require('cors')
+
+app.use(cors())
 app.use(express.json());
 
 morgan.token("content", (req) => {
